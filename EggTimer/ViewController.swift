3f7e14c8eae181c2a7225457cf9e192d1ea250e2
Archivaldi <artur.markov1860@gmail.com>
2020-11-24 22:46:41 -0600

    alarm sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var TextLabel: UILabel!
     
     @IBOutlet weak var progressBar: UIProgressView!
+    
+    var player: AVAudioPlayer!
     
     let eggTimes: [String: Int] = ["Soft": 5, "Medium": 8, "Hard": 12];
     var totalTime: Int = 0
@@ -23,13 +26,14 @@ class ViewController: UIViewController {
 //        let hardness = sender.currentTitle!
 //            print(eggTimes[hardness]!)
 //        }
-        TextLabel.text = "How do you like your eggs?"
         secondsPassed = 0;
+        progressBar.progress = 0.0
         timer.invalidate()
         
         if let hardness = sender.currentTitle {
             if let answer = eggTimes[hardness] {
                 totalTime = answer
+                TextLabel.text = hardness
             }
         }
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startTimer), userInfo: nil, repeats: true)
@@ -37,15 +41,23 @@ class ViewController: UIViewController {
     
     
     @objc func startTimer() {
-        if secondsPassed != totalTime {
+        if secondsPassed < totalTime {
             secondsPassed += 1
             progressBar.progress = Float(secondsPassed)/Float(totalTime)
         } else {
             timer.invalidate()
             TextLabel.text = "Done!"
+            playSound()
         }
         
     }
+    
+    func playSound() {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
 }
 
 
