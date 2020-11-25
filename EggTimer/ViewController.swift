5e698f4c4d6c1f7985dcf3e1b0804d6acfbb6e89
Archivaldi <artur.markov1860@gmail.com>
2020-11-24 22:03:00 -0600

    timer and label

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes: [String: Int] = ["Soft": 5, "Medium": 8, "Hard": 12];
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    
+    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 480, "Hard": 720];
+    var timeLeft: Int = 60
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
 //        let hardness = sender.currentTitle!
 //            print(eggTimes[hardness]!)
 //        }
+        timer.invalidate()
+        
         if let hardness = sender.currentTitle {
             if let answer = eggTimes[hardness] {
-                print(answer)
+                timeLeft = answer
             }
         }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func startTimer() {
+        if timeLeft > 0 {
+            print("\(timeLeft) seconds")
+            timeLeft -= 1
+        } else {
+            TextLabel.text = "Done!"
+        }
+        
     }
 }
+
+
